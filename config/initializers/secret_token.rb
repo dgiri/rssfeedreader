c57068c1ea9583acd900ff820c8acf2da67c3d84
Debashis Giri <debashis.giri@tiatros.com>
2014-09-13 20:56:36 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rssfeedreader::Application.config.secret_token = '028f3e6828cd152ed94debff7b9c984a97f5ddb874e7199208b8e21987c5e8ecc98e1d42a3ce14d21a908ddf1a2ee900e8b6c3bb13cd2cbb54a6d6c97e97809c'
