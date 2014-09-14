462ccc66240d92bf9fff53b8ceed70a85347580f
Debashis Giri <debashis.giri@tiatros.com>
2014-09-14 10:42:34 +0530

    error handling added

--- app/controllers/rssfeeds_controller.rb
+++ app/controllers/rssfeeds_controller.rb
@@ -9,13 +9,17 @@ class RssfeedsController < ApplicationController
 			# example url http://rss.news.yahoo.com/rss/entertainment / https://www.ruby-lang.org/en/feeds/news.rss
 
 			@rssitems = []
-			open(url) do |rss|
-			  feed = RSS::Parser.parse(rss)
-			  @title = feed.channel.title
+			begin
+				open(url) do |rss|
+				  feed = RSS::Parser.parse(rss)
+				  @title = feed.channel.title
 
-			  feed.items.each do |item|
-			  	@rssitems << item
-			  end
+				  feed.items.each do |item|
+				  	@rssitems << item
+				  end
+				end
+			rescue => error
+			  @errors = "Url are not proper."
 			end
 		end
 		respond_to do |format|
@@ -27,20 +31,22 @@ class RssfeedsController < ApplicationController
 	def withfeedjira
 		if params[:url] && !params[:url].blank?
 			url = params[:url]
-			feed = Feedjira::Feed.fetch_and_parse url
-			unless feed == 0
-				@title = feed.title
-				@rssitems = feed.entries
-			else
-				@error = "Input url is not valid."
+			begin
+				feed = Feedjira::Feed.fetch_and_parse url
+				unless feed == 0
+					@title = feed.title
+					@rssitems = feed.entries
+				else
+					@error = "Input url is not valid."
+				end
+			rescue => error
+			  @errors = "Url are not proper."
 			end
 		end
-
 
 		respond_to do |format|
 	      format.html # index.html.erb
 	      format.js {}
 	  end
-
 	end
 end
